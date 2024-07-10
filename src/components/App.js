50232b2908f4015938dd363cb13c1706ead7635b
Razzakh <mrazzakh123@gmail.com>
2024-07-10 11:00:21 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,15 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
-
+const [btnclick,setbtnclick]=useState('hide');
+const handlebtn=()=>{
+  setbtnclick('show');
+}
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+    <p id="para" className={btnclick} >Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      <button onClick={handlebtn} id="click">Click to see para</button>
+      {/* // Do not alter the main div */}
     </div>
   );
 }
